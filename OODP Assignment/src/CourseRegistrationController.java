112ee71eb61385ed460ffefef075281a432dfaa5
jwenT <56579875+jwenT96@users.noreply.github.com>
2020-11-14 02:56:26 +0800

    For the lists of both Course&Student, it's currently just a temp code
    In progress

---
+++ OODP Assignment/src/CourseRegistrationController.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+
+public class CourseRegistrationController {
+	
+	//checks if the course code is valid
+	public boolean checkCourseCode(String courseCode){
+	    //Using this temp code for the lists first
+		ArrayList<Course> courseList = new ArrayList<Course>();
+		
+		for(int i=0;i<courseList.size();i++) {
+			//get courseList
+			if (courseList.get(i).getCourseCode()== courseCode)
+				return true;	
+		}
+		
+		return false;
+		
+	}
+	
+	//prints the indexes for the particular course and vacancies for users to choose from
+	public int showCourseIndexNVacancies(String courseCode) {
+		
+		ArrayList<CourseIndex> indexList = new ArrayList<CourseIndex>();
+		for(int i=0;i<indexList.size();i++) {
+			//Print index number 
+			System.out.print("("+ (i+1)+"):" + indexList.get(i).getIndexNo() + ", Vacancies: " + indexList.get(i).getNoOfVacancies());
+		}
+		
+		return indexList.size();		
+		
+	}
+	
+	//Adds the student into the Student array
+	public void registerStudentforCourse(String courseCode,int i,Student student){
+		ArrayList<CourseIndex> indexList = new ArrayList<CourseIndex>();
+		if (indexList.get(i-1).getNoOfVacancies()!=0) {
+			indexList.get(i-1).addStudentList(student);
+			indexList.get(i-1).setNoofVacancies(-1); // Remove 1 vacancy
+			System.out.print("Registration is successful.");
+		}
+		else {
+			System.out.print("Please choose an index with vacancies");
+		}
+	}
+		
+	//Check if the student is already registered in that course
+	public boolean checkIfRegistered(String courseCode,Student student) {
+		ArrayList<CourseIndex> indexList = new ArrayList<CourseIndex>();
+		for(int i=0; i<indexList.size();i++) {
+			ArrayList<Student> studentList= indexList.get(i).getStudentList();
+			if (studentList.contains(student)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
---
+++ OODP Assignment/src/CourseRegistrationUI.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class CourseRegistrationUI{
+	Scanner sc = new Scanner(System.in);
+	
+	public void registerforCourse(){
+		boolean valid=true;
+		//Controller used
+		CourseRegistrationController CRC= new CourseRegistrationController();
+		Student student=new Student();
+		
+		System.out.print("Registering Course---------------");
+		while (valid) {
+			System.out.print("Enter Course Code:");
+			String coursecode = sc.next();
+		
+			//Check if coursecode is a valid course code
+			if (CRC.checkCourseCode(coursecode)==true && CRC.checkIfRegistered(coursecode, student)) {
+				//print Course Indexes list
+				int noofIndex=CRC.showCourseIndexNVacancies(coursecode);
+				System.out.print("Select index to register:");
+				int userinput= sc.nextInt();
+				CRC.registerStudentforCourse(coursecode,userinput,student);	
+				
+		}
+			
+			else if (CRC.checkCourseCode(coursecode)==false) {
+				System.out.print("Please enter a correct course code.");
+				continue;
+			}
+			else {
+				System.out.print("You are already registered for this course.");
+				}
+		}
+		
+		//Return to StudentUI? 
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+
+}
