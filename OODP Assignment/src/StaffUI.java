096f53f5916971fe008cca952678af31e6ca7e09
TeoZhiKai <73381439+TeoZhiKai@users.noreply.github.com>
2020-11-14 14:19:59 +0800

    Update StaffUI.java

--- OODP Assignment/src/StaffUI.java
+++ OODP Assignment/src/StaffUI.java
@@ -11,8 +11,24 @@ public class StaffUI {
 	public StaffUI(Staff staff) {
 		this.staff = staff;
 	}
+	
+	
 	public void initStaffUI() {
+		
+		do{
 		System.out.println("Welcome to STARS (Staff): "+this.staff.getUsername());
+		System.out.println("1. Print Studentlist in the course");	
+		choice = sc.nextInt();
+		
+		switch (choice) {
+		case 1: 
+			System.out.println("Pls enter course code (e.g. CZ2002)");
+			String code = sc.next();
+			System.out.println("Pls enter course index no. (e.g. 10198)");
+			int index = sc.nextInt();
+			
+			printslist.printStudentList(code);
+			
 		System.exit(0);//temp code
 	}
 
