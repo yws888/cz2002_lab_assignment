6641a0cc90f052a4cf4c06a5f37f61a41975370c
AlvinTang81 <74127963+AlvinTang81@users.noreply.github.com>
2020-11-20 19:31:04 +0800

    Update LoginUI.java

--- OODP Assignment/src/boundary/LoginUI.java
+++ OODP Assignment/src/boundary/LoginUI.java
@@ -12,70 +12,93 @@ public class LoginUI {
 		LoginController logincontroller = new LoginController();
 		String username, password;
 		int userinput;
+		
 		boolean verifylogin;
 		UserUI userUI;
-		Scanner sc = new Scanner(System.in);	
+		
+		
 		while(true) {
+			
 			System.out.println("====MySTARS====");
 			System.out.println("1. Login");
 			System.out.println("2. Exit");
-
-			userinput = sc.nextInt();
-			switch(userinput) {
-			case 1:
-				System.out.println("Username:");
-				username = sc.next();
-				Console cnsl = System.console();
-				if (cnsl == null) { //eclipse doesnt support input masking
-					System.out.println("Password:");
-					password = sc.next();
-			    } 
-				else {
-					char[] ch = cnsl.readPassword( "Enter password : ");
-					password = new String(ch);
-				}
-				//System.out.println("Username: "+username+ " Password: "+password);
-				System.out.println("Processing....");
-				verifylogin = logincontroller.verifylogin(username,password);
-				if(verifylogin == true) {
-					int mode = logincontroller.getLoginMode(username);
-					switch(mode) {
-						case 1: //Student
-							userUI = new StudentUI(logincontroller.getStudent(username));
-							
-							if (LoginController.isValidAccessTime()==true){
-								System.out.println("Login Successful.");
-								((StudentUI) userUI).initStudentUI();
-								}
-							else
-							{
-								System.out.println("Sorry you are not allowed to access the portal now!");
-								System.out.println("Please log in at your specified access period!");
-								System.out.println("Your access period is on " +LoginController.retriveAccessperiod());
-							}
-							
-							break;
-						case 2: //Staff
-							userUI = new StaffUI(logincontroller.getStaff(username));
-							System.out.println("Login Successful.");
-							((StaffUI) userUI).initStaffUI();
-							break;
-						case 3: //error
-							System.out.println("Error, could not find user entry.");
-							break;
-					}
-				}else {
-					System.out.println("Login Failed.");
-				}
+			
+			Scanner sc = new Scanner(System.in);
+			
+			if(sc.hasNextInt()) {
 				
-				break;
-			case 2:
-				sc.close();
-				System.exit(0);
-				break;
-
+				userinput = Integer.parseInt(sc.next());
+						
+				switch(userinput) {
+				case 1:
+					System.out.println("Username:");
+					username = sc.next();
+					Console cnsl = System.console();
+					if (cnsl == null) { //eclipse doesnt support input masking
+						System.out.println("Password:");
+						password = sc.next();
+				    } 
+					else {
+						char[] ch = cnsl.readPassword( "Enter password : ");
+						password = new String(ch);
+					}
+					//System.out.println("Username: "+username+ " Password: "+password);
+					System.out.println("Processing....");
+					verifylogin = logincontroller.verifylogin(username,password);
+					if(verifylogin == true) {
+						int mode = logincontroller.getLoginMode(username);
+						switch(mode) {
+							case 1: //Student
+								userUI = new StudentUI(logincontroller.getStudent(username));
+								
+								if (LoginController.isValidAccessTime()==true){
+									System.out.println("Login Successful.");
+									((StudentUI) userUI).initStudentUI();
+									}
+								else
+								{
+									System.out.println("Sorry you are not allowed to access the portal now!");
+									System.out.println("Please log in at your specified access period!");
+									System.out.println("Your access period is on " +LoginController.retriveAccessperiod());
+								}
+								
+								continue;
+							case 2: //Staff
+								userUI = new StaffUI(logincontroller.getStaff(username));
+								System.out.println("Login Successful. ");
+								((StaffUI) userUI).initStaffUI();							
+								continue;
+							case 3: //error
+								System.out.println("Error, could not find user entry.");
+								break;
+						}
+					}else {
+						System.out.println("Login Failed.");
+						System.out.println();
+						break;
+					}
+					
+					break;
+				//exit system
+				case 2:
+					sc.close();
+					System.out.println("Thank you for using MyStars, goodbye!");
+					System.exit(0);
+					break;
+				
+				//default error input message
+				default: 
+					System.out.println("Please select option 1 or 2");
+					System.out.println();
+					
+					continue;
+				}
+							
 			}
-
+			
+			System.out.println("Please select option 1 or 2");
+			System.out.println();
+			continue;
 		}
 		
 	}
