7abf48fdd1417d3bb9681002cbb56e13e4afa51c
AlvinTang81 <74127963+AlvinTang81@users.noreply.github.com>
2020-11-23 12:28:34 +0800

    Update StudentUI.java

--- OODP Assignment/src/boundary/StudentUI.java
+++ OODP Assignment/src/boundary/StudentUI.java
@@ -1,7 +1,6 @@
 package boundary;
 import java.util.Scanner;
 
-//import control.CourseMgr;
 import control.StudentController;
 import entity.Student;
 
@@ -38,54 +37,56 @@ public class StudentUI implements UserUI {
 	/**
 	 * User Interface for Student 
 	 */
-	public void initUI() {
+	public void initStudentUI() {
 		int choice = -1;
 
 		Scanner sc = new Scanner(System.in);
 		
 		do {
-		System.out.println("\nWelcome to STARS (Student): "+this.student.getName());
-		System.out.println("1. Add Course");
-		System.out.println("2. Drop Course");
-		System.out.println("3. Check/Print Courses Registered");
-		System.out.println("4. Check Vacancies Available");
-		System.out.println("5. Change Index Number of Course");
-		System.out.println("6. Swap Index Number with Another Student");
-		System.out.println("0. Exit");
-		System.out.print("\nEnter the number of your choice: "); 
-		try {
-			choice = Integer.parseInt(sc.nextLine());
-			
-			switch (choice) {
-			case 1:
-				StudentController.addCourse(student);
-				break;
-			case 2:
-				StudentController.dropCourse(student);
-				break;
-			case 3:
-				StudentController.printRegisteredCourses(student);
-				break;
-			case 4:
-				StudentController.checkCourseVacancy();
-				break;
-			case 5:
-				StudentController.changeIndex(student);
-				break;
-			case 6:
-				StudentController.swapIndex(student);
-				break;
-			case 0: //quit
-				break;
-			default:
-				System.out.println("Please select an option from 0-6");
-				System.out.println();
-				break;
-}
-		} catch (NumberFormatException e) {
-			System.out.println("Please select an option from 0-6");
-		}
-} while (choice != 0);
+				System.out.println("\nWelcome to STARS (Student): "+this.student.getName());
+				System.out.println("1. Add Course");
+				System.out.println("2. Drop Course");
+				System.out.println("3. Check/Print Courses Registered");
+				System.out.println("4. Check Vacancies Available");
+				System.out.println("5. Change Index Number of Course");
+				System.out.println("6. Swap Index Number with Another Student");
+				System.out.println("0. Exit");
+				System.out.print("\nEnter the number of your choice: "); 
+				try {
+					choice = Integer.parseInt(sc.nextLine());
+					
+					switch (choice) {
+					case 1:
+						StudentController.addCourse(student);
+						break;
+					case 2:
+						StudentController.dropCourse(student);
+						break;
+					case 3:
+						StudentController.printRegisteredCourses(student);
+						break;
+					case 4:
+						StudentController.checkCourseVacancy();
+						break;
+					case 5:
+						StudentController.changeIndex(student);
+						break;
+					case 6:
+						StudentController.swapIndex(student);
+						break;
+					case 0: //quit
+						break;
+					default:
+						System.out.println("Please select an option from 0-6");
+						System.out.println();
+						break;
+					}
+			} 
+			catch (NumberFormatException e) {
+			System.out.println("Please select a valid numeric option");
+			System.out.println();
+			} 
+		} while (choice != 0);
 
 		return;
 	}
